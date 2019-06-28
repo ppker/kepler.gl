88bd2e2c1a3c9f3c42ee4fc5fdd8b07d385751e8
Giuseppe Macri <gmacri@uber.com>
2019-06-28 14:13:32 -0700

    TEMP: working
    Signed-off-by: Giuseppe Macri <gmacri@uber.com>

--- src/reducers/ui-state.js
+++ src/reducers/ui-state.js
@@ -49,10 +49,12 @@ const actionHandler = {
   [ActionTypes.SET_EXPORT_FILTERED]: uiStateUpdaters.setExportFilteredUpdater,
   [ActionTypes.SET_EXPORT_DATA]: uiStateUpdaters.setExportDataUpdater,
   [ActionTypes.SET_USER_MAPBOX_ACCESS_TOKEN]: uiStateUpdaters.setUserMapboxAccessTokenUpdater,
+
   [ActionTypes.SET_EXPORT_MAP_FORMAT]: uiStateUpdaters.setExportMapFormat,
-  [ActionTypes.SET_MAP_MODE]: uiStateUpdaters.setMapModeUpdater,
   [ActionTypes.SET_FEATURES]: uiStateUpdaters.setFeaturesUpdater,
-  [ActionTypes.SET_EXPORT_MAP_HTML_MODE]: uiStateUpdaters.setExportMapHTMLMode
+  [ActionTypes.SET_EXPORT_MAP_HTML_MODE]: uiStateUpdaters.setExportMapHTMLMode,
+  [ActionTypes.SET_EDITOR_MODE]: uiStateUpdaters.setEditorModeUpdater
+
 };
 
 /* Reducer */
